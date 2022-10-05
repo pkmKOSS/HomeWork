f1afd3db1b5e11fa110a3ffccf1ddb4796820d2c
Григоренко Александр Игоревич <aigrigorenko@mtsbank.ru>
2022-10-05 09:08:55 +0500

    Lesson-12

---
+++ task3-4/task3-4/Localizble/Strings.swift
@@ -0,0 +1,23 @@
+//
+//  Strings.swift
+//  task3-4
+//
+//  Created by Григоренко Александр Игоревич on 05.10.2022.
+//
+
+/// Константы со строками.
+enum Strings {
+    enum ColorsNames {
+        static let red = "Красный"
+        static let blue = "Голубой"
+        static let yellow = "Желтый"
+        static let black = "Черный"
+    }
+
+    enum TextForAlert {
+        static let atentionString = "Внимание"
+        static let inputDataString = "Введите данные"
+        static let someDataString = "Какие-то данные"
+        static let enterString = "Ввод"
+    }
+}
